ac29e7c05894011629742dc52ae1e12c8c3c7db8
Tam Ton <107794321+tamton23@users.noreply.github.com>
2024-12-20 22:47:27 +0700

    Delete group_data.py

--- group_data.py
+++
@@ -1,7 +0,0 @@
-def get_label_index(labels):
-	from collections import defaultdict
-	label_index = defaultdict(list)
-	for index, label in enumerate(labels):
-		label_index[label].append(index)
-	return label_index
-
