d634e5349271f2cf7776fe25bb0eb1497f47a110
Tam Ton <107794321+tamton23@users.noreply.github.com>
2024-12-20 22:47:46 +0700

    Delete loadHamEmails_labels.py

--- loadHamEmails_labels.py
+++
@@ -1,8 +0,0 @@
-import glob
-import os
-file_path = 'enron1/ham/'
-for filename in glob.glob(os.path.join(file_path, '*.txt')):
-        with open(filename, 'r', encoding = "ISO-8859-1") as infile:
-                e_mails.append(infile.read())
-                labels.append(0)
-
